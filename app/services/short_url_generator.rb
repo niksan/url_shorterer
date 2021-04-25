50e5049bcad34efe4edba772e7aae189372400d5
Aleksander Nikulin <nikulinaleksandr@gmail.com>
2021-04-25 13:02:53 +0300

    fix: move logic to separate method

--- app/services/short_url_generator.rb
+++ app/services/short_url_generator.rb
@@ -7,12 +7,16 @@ class ShortUrlGenerator < ApplicationService
   end
 
   def call
-    Digest::SHA1.hexdigest(secret_string)[START_POS..END_POS]
+    generate_short_code
   end
 
   private
 
   attr_reader :url
+
+  def generate_short_code
+    Digest::SHA1.hexdigest(secret_string)[START_POS..END_POS]
+  end
 
   def secret_string
     url + Time.current.to_s
